77cd88112cb39b8dc84ed42a1c956da58b48ea9a
geo8bit <geo8bit@gmail.com>
2013-05-30 20:27:45 +0300

    First commit with configuration file.Bug in resourcer

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
-nodebob
----
+#nodebob
 
-nodebob is a **build tool** for **node-webkit** written in windows batch script.
+nodebob is a **build tool** for **node-webkit** written in nodejs.
 
-Write your node-webkit project inside the ```app``` folder and build your native win application in just one click!
+Write your node-webkit project inside the `app` folder and build your desktop application in just one click!
 
-* nodebob creates the folder ```release``` that contains all the necessary files to build a standalone application without dependencies
-* you can also add your custom application icon in ```app/app.ico```
+* nodebob creates the folder `output` that contains all the necessary files to build a standalone application without dependencies
+* you can also add your custom application icon in `app/app.ico`
     
-**node-webkit** is an app runtime based on ```Chromium``` and ```node.js```.
+**node-webkit** is an app runtime based on `Chromium` and `node.js`.
 
 > You can write native apps in HTML and Javascript with node-webkit. It also lets you to call Node.js modules directly from DOM and enables a new way of writing native applications with all Web technologies.
 
@@ -21,26 +20,45 @@ Write your node-webkit project inside the ```app``` folder and build your native
 * Easy to package and distribute apps.
 * Available on Linux, Mac OSX and Windows
 
-> [source] [1]
+> [source] [node-webkit]
 
-Tech
--
+##Configuration
+
+You may save different build profiles in `config.json` and switch between them by changing the `activeProfile` property. Paths are relatve, remember to escape backslash. The configuration options are:
+
+**appName**: Your application's name. Will name the executable. *Default: `'app'`*  
+**appPath**: Path to your app's root folder. *Default: `'app'`*  
+**appIco**: Path to your ico. *Default: `'app\app.ico'`*  
+**outputPath**: Path to output folder. *Default: `'output'`*  
+**packagePath**: Path to the package file. *Default: `'output\app.nw'`*  
+**ffmpegsumo**: Wether to include `ffmpegsumo.dll` in your app. *Default: `true`*  
+**libEGL**: Wether to include `libEGL.dll` in your app. *Default: `false`*  
+**libGLESv2**: Wether to include `libGLESv2.dll` in your app. *Default: `false`*  
+**keepPackage**: Wether to keep or delete the package file (.nw). *Default: `false`*
+
+Include `ffmpegsumo.dll` in your output if you want to use `<video>` and `<audio>` tags. Include `libEGL.dll` and `libGLESv2.dll` if you are using WebGL.
+
+##Tech
+
 nodebob uses a number of open source projects to work properly:
 
 * [7-zip] - a lightware file archiver, v9.20
 * [Anolis Resourcer] - a windows resource editor, v0.9.0
 * [node-webkit] - v0.5.1-win32
 
-Version
--
-0.1
+##Dependencies
 
-Windows versions
--
+You need nodejs installed in your system.
+
+##Version
+
+0.2.0
+
+##Windows versions
+
 Tested on ```Windows 7 x64```.
 
-Quick start
--
+##Quick start
 
 **Install**
 
@@ -50,12 +68,12 @@ Quick start
 
 Execute ```nodebob/build.bat```
 
-License
--
-[GPLv2]
+##License
+
+[MIT]
 
 [node-webkit]: https://github.com/rogerwang/node-webkit
 [7-zip]: http://www.7-zip.org/
 [Anolis Resourcer]: http://www.anol.is/
-[GPLv2]: http://www.gnu.org/licenses/gpl-2.0.html
-[1]: https://github.com/rogerwang/node-webkit#introduction 
+[MIT]: http://opensource.org/licenses/MIT
+[node-webkit]: https://github.com/rogerwang/node-webkit#introduction 
--- buildTools/nb/nodebob.js
+++ buildTools/nb/nodebob.js
@@ -1,58 +1,108 @@
-shell = require('shelljs');
+var shell = require('shelljs'),
+	fs = require('fs');
 
 var echo = console.log;
 
-// PATHS
-
-var paths = {};
-paths.basePath = function () {
-	r = shell.pwd().split('\\');
-	r = r.splice(0,r.length-2).join('\\');
-	r += '\\';
-	return r
-}();
-paths.packagePath 	= paths.basePath + 'release\\app.nw';
-paths.exePath 		= paths.basePath + 'release\\app.exe';
-paths.icoPath 		= paths.basePath + 'app\\app.ico';
-paths.nwPath 		= paths.basePath + 'buildTools\\nw\\nw.exe';
-paths.appPath 		= paths.basePath + 'app\\*';
-
-// COMMANDS
-
-var commandsCnt = function() {
-	this.zip		= paths.basePath + 'buildTools\\7z\\' + '7z a -tzip ' + paths.packagePath + ' ' + paths.appPath;
-	this.ar			= paths.basePath + 'buildTools\\ar\\' + 'resourcer -op:upd -src:' + paths.exePath + ' -type:14 -name:IDR_MAINFRAME -file:' + paths.icoPath;
-	this.copy		= 'copy /b /y ' + paths.nwPath + ' ' + paths.exePath;
-	this.merge		= 'copy /b /y ' + paths.exePath + ' + ' + paths.packagePath + ' ' + paths.exePath;
-	this.ffmpegsumo = 'copy /y ' + paths.basePath + '\\buildTools\\nw\\ffmpegsumo.dll ' + paths.basePath + '\\release\\ffmpegsumo.dll';
-	this.libEGL 	= 'copy /y ' + paths.basePath + '\\buildTools\\nw\\libEGL.dll ' + paths.basePath + '\\release\\libEGL.dll';
-	this.libGLESv2 	= 'copy /y ' + paths.basePath + '\\buildTools\\nw\\libGLESv2.dll ' + paths.basePath + '\\release\\libGLESv2.dll';
-	this.icudt 		= 'copy /y ' + paths.basePath + '\\buildTools\\nw\\icudt.dll ' + paths.basePath + '\\release\\icudt.dll';
-	this.nwpak 		= 'copy /y ' + paths.basePath + '\\buildTools\\nw\\nw.pak ' + paths.basePath + '\\release\\nw.pak';
-	this.del 		= 'del ' + paths.packagePath;
-	this.exec 	= function(c,m) {
-		if (m) echo(m);
-		shell.exec(this[c]);
+function fixSpaces(arr) {
+	for (i in arr) {
+		if (arr[i].indexOf(' ') > -1) {
+			arr[i] = arr[i].replace(' ','');
+			arr[i] = arr[i].splice(0,6) + '~1';
+		}
 	}
+	return arr;
 }
 
-// MAIN
+function initConfig(config) {
+	var profile = config.profiles[config.activeProfile];
+	this.appName		= profile.appName || this.appName;
+	this.appPath		= profile.appPath || this.appPath;
+	this.appIco			= profile.appIco || this.appIco;
+	this.outputPath 	= profile.outputPath || this.outputPath;
+	this.ffmpegsumo		= profile.ffmpegsumo || this.ffmpegsumo;
+	this.libEGL			= profile.libEGL || this.libEGL;
+	this.libGLESv2		= profile.libGLESv2 || this.libGLESv2;
+	this.keepPackage	= profile.keepPackage || this.keepPackage;
+	this.packagePath	= profile.packagePath || this.packagePath;
+	return this;
+}
 
-shell.cd(paths.basePath);
-var command = new commandsCnt();
+// H/C default configuration
+initConfig.prototype = {
+    "appName"		: "app",
+    "appPath"		: "app",
+    "appIco"		: "app\\app.ico",
+    "outputPath"	: "output",
+	"packagePath"	: "output\\app.nw",
+    "ffmpegsumo"	: true,
+    "libEGL"		: false,
+    "libGLESv2"		: false,
+    "keepPackage"	: false
+};
 
-echo('\n..:: nodebob v0.1 ::..');
-echo('\na build tool for node-webkit');
+function initPaths(profile) {
+	var paths = {};
+	paths.basePath = function () {
+		bP = shell.pwd().split('\\');
+		bP = bP.splice(0,bP.length-2);
+		//bP = fixSpaces(bP);
+		bP = bP.join('\\');
+		return bP;
+	}();
+	paths.packagePath 	= [paths.basePath, profile.packagePath].join('\\');
+	paths.exePath 		= [paths.basePath, profile.outputPath, profile.appName + '.exe'].join('\\');
+	paths.icoPath 		= [paths.basePath, profile.appIco].join('\\');
+	paths.nwPath 		= [paths.basePath, 'buildTools', 'nw', 'nw.exe'].join('\\');
+	paths.appPath 		= [paths.basePath, profile.appPath, '*'].join('\\');
+	return paths;
+}
 
-command.exec('zip', '\n..:: Creating package ::..');
-command.exec('copy','\n..:: Creating exec ::..');
-command.exec('ar');
-command.exec('merge');
-command.exec("icudt", '\n..:: Copying files ::..');
-command.exec("nwpak");
-command.exec("ffmpegsumo");
-command.exec("libEGL");
-command.exec("libGLESv2");
-command.exec("del", '\n..:: Deleting temporary files ::..');
+function initCommands(profile) {
+	var result = {
+		zip			: [paths.basePath + '\\buildTools\\7z\\' + '7z a -tzip', paths.packagePath, paths.appPath],
+		res			: [paths.basePath + '\\buildTools\\ar\\' + 'resourcer -op:upd -src:' + paths.exePath, '-type:14 -name:IDR_MAINFRAME -file:' + paths.icoPath],
+		copy		: ['copy /y', paths.nwPath, paths.exePath],
+		merge		: ['copy /y /b', paths.nwPath, '+', paths.packagePath, paths.exePath],
+		ffmpegsumo 	: ['copy /y', paths.basePath + '\\buildTools\\nw\\ffmpegsumo.dll', paths.basePath + '\\' + profile.outputPath + '\\ffmpegsumo.dll'],
+		libEGL 		: ['copy /y', paths.basePath + '\\buildTools\\nw\\libEGL.dll', paths.basePath + '\\' + profile.outputPath + '\\libEGL.dll'],
+		libGLESv2 	: ['copy /y', paths.basePath + '\\buildTools\\nw\\libGLESv2.dll', paths.basePath + '\\' + profile.outputPath + '\\libGLESv2.dll'],
+		icudt 		: ['copy /y', paths.basePath + '\\buildTools\\nw\\icudt.dll', paths.basePath + '\\' + profile.outputPath + '\\icudt.dll'],
+		nwpak 		: ['copy /y', paths.basePath + '\\buildTools\\nw\\nw.pak', paths.basePath + '\\' + profile.outputPath + '\\nw.pak'],
+		del 		: ['del', paths.packagePath],
+		exec 		: function(c,m) {
+			if (m) echo(m);
+			shell.exec(this[c].join(' '));
+		}
+	}
+	return result;
+}
 
-echo('\n..:: Done!');
+
+fs.readFile('../../config.json', 'utf8', function (err,data) {
+	if (err) return console.log(err);
+	echo('\n..:: nodebob v0.2 ::..');
+	echo('\na build tool for node-webkit');
+
+	// get user options
+	var config = JSON.parse(data);
+	var profile = new initConfig(config);
+
+	// init 
+	paths = initPaths(profile);
+	commands = initCommands(profile);
+
+	// run 	/*
+	shell.cd(paths.basePath);
+	commands.exec('zip', '\n:: Creating package ::...');
+	commands.exec('copy','\n:: Creating executable ::...');
+	commands.exec('res');
+	commands.exec('merge');
+	commands.exec("icudt", '\n:: Copying files ::...');
+	commands.exec("nwpak");
+	if (profile.ffmpegsumo)		commands.exec("ffmpegsumo");
+	if (profile.libEGL) 		commands.exec("libEGL");
+	if (profile.libGLESv2) 		commands.exec("libGLESv2");
+	if (!profile.keepPackage)	commands.exec("del", '\n:: Deleting temporary files ::...\n');
+
+	echo('\n:: Done! ::'); /**/
+});
