32eed43efe309c035bbf352e4abd2dba0a5423f0
Michael Yin <layerssss@gmail.com>
2014-04-05 16:21:58 +0800

    building on osx

---
+++ build.osx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+echo "nodebob v0.1"
+echo "---"
+echo
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p $CUR_DIR/release.osx
+
+echo "Extractiong node-webkit..."
+rm -Rf $CUR_DIR/release.osx/nw
+unzip $CUR_DIR/buildTools/nw.osx/node-webkit-v0.8.5-osx-ia32.zip -d $CUR_DIR/release.osx/nw > /dev/null
+echo "Creating bundle for osx..."
+rm -Rf $CUR_DIR/release.osx/app.app
+cp -R $CUR_DIR/release.osx/nw/node-webkit.app $CUR_DIR/release.osx/app.app
+cp -R $CUR_DIR/app $CUR_DIR/release.osx/app.app/Contents/Resources/app.nw
+cp app/Info.plist $CUR_DIR/release.osx/app.app/Contents/Info.plist
+sips -s format tiff $CUR_DIR/app/app.ico --out $CUR_DIR/release.osx/app_icon.tiff > /dev/null
+tiff2icns -noLarge $CUR_DIR/release.osx/app_icon.tiff $CUR_DIR/release.osx/app.app/Contents/Resources/nw.icns
+echo "Deleting temporary files..."
+rm -Rf $CUR_DIR/release.osx/nw $CUR_DIR/release.osx/app_icon.tiff
+echo "Done!"
